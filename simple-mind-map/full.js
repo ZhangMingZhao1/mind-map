a79550f8412e3dfeb8355cb28ab9245f7c79feeb
zhangmingzhao <zhangmingzhao@zuoyebang.com>
2025-02-19 16:20:42 +0800

    version: 更改版本

--- simple-mind-map/full.js
+++ simple-mind-map/full.js
@@ -30,7 +30,7 @@ MindMap.markdown = markdown
 MindMap.iconList = icons.nodeIconList
 MindMap.constants = constants
 MindMap.defaultTheme = defaultTheme
-MindMap.version = '0.0.14'
+MindMap.version = '0.0.16'
 
 MindMap.usePlugin(MiniMap)
   .usePlugin(Watermark)
